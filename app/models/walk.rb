870cebc63893d35b87b0a9ec6cad114040597ccd
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2025-01-06 15:47:46 +0100

    fix error

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,5 +1,5 @@
 class Walk < ApplicationRecord
-  has_many :user_walks
+  has_many :user_walks, dependent: :destroy
   has_many :users, through: :user_walks
 
   validates :number_of_dog, presence: true, numericality: { greater_than: 0 }

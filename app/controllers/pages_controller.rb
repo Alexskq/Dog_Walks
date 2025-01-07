c3876d33c221892ec81e8a607242072255a8bbc6
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2025-01-07 22:14:38 +0100

    add profil page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
     # les 3 dernieres walks créees
     @closest_walks = Walk.order('created_at DESC').limit(3)
   end
+
+  def profil
+    @user = current_user
+    @walks = Walk.where(user: current_user)
+  end
 end

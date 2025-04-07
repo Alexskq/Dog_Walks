dfefd336b28801ff4eec80457aeec6af19823cd0
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2025-04-07 13:19:40 +0200

    update avatar error in navbar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,9 +12,14 @@ module Users
     # end
 
     # POST /resource
-    # def create
-    #   super
-    # end
+    def create
+      super do |resource|
+        if resource.persisted? && !resource.avatar.attached?
+          # Attach a default avatar if none is provided
+          # This is optional - you can remove this if you don't want to attach a default avatar
+        end
+      end
+    end
 
     # GET /resource/edit
     # def edit
@@ -57,6 +62,11 @@ module Users
 
     # protected
 
+    # The path used after sign up.
+    def after_sign_up_path_for(resource)
+      root_path
+    end
+
     # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_up_params
       devise_parameter_sanitizer.permit(:sign_up,
@@ -74,11 +84,6 @@ module Users
     def build_dog
       build_resource.dog ||= Dog.new
     end
-
-    # The path used after sign up.
-    # def after_sign_up_path_for(resource)
-    #   super(resource)
-    # end
 
     # The path used after sign up for inactive accounts.
     # def after_inactive_sign_up_path_for(resource)
